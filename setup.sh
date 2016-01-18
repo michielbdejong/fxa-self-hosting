394c018683c3d150d65d693ec32217577f5a5491
Michiel de Jong <mbdejong@mozilla.com>
2016-01-18 22:01:26 +0100

    Work on syncto

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ echo Building Mozilla images - skipping
 # docker build -f ./docs/self-host.docker -t fxa-oauth-server https://github.com/michielbdejong/fxa-oauth-server.git#docker
 # docker build -f ./docs/self-host.docker -t browserid-verifier https://github.com/michielbdejong/browserid-verifier.git#docker
 # docker build -f ./docs/self-host.docker -t fxa-profile-server https://github.com/michielbdejong/fxa-profile-server.git#docker
-# docker build -t syncserver https://github.com/mozilla-services/syncserver.git
+# docker build -t syncserver https://github.com/michielbdejong/syncserver.git#docker
 # docker build -t syncto https://github.com/michielbdejong/syncto.git#docker
 # docker build -t fxa-self-hosting https://github.com/michielbdejong/fxa-self-hosting.git
 
