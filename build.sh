39df7af8c79d0612859dd10e81ee7a2fcc3b95f7
Michiel de Jong <mbdejong@mozilla.com>
2016-01-27 10:10:48 +0100

    Three PRs have been merged

--- build.sh
+++ build.sh
@@ -21,11 +21,11 @@ docker pull nginx
 echo Building images...
 
 docker build -f ./docs/self-host.docker -t fxa-content-server https://github.com/mozilla/fxa-content-server.git
-docker build -f ./docs/self-host.docker -t fxa-auth-server https://github.com/michielbdejong/fxa-auth-server.git#docker
+docker build -f ./docs/self-host.docker -t fxa-auth-server https://github.com/mozilla/fxa-auth-server.git
 docker build -f ./docs/self-host.docker -t fxa-auth-db-mysql https://github.com/mozilla/fxa-auth-db-mysql.git
-docker build -f ./docs/self-host.docker -t fxa-oauth-server https://github.com/michielbdejong/fxa-oauth-server.git#docker
+docker build -f ./docs/self-host.docker -t fxa-oauth-server https://github.com/mozilla/fxa-oauth-server.git
 docker build -t browserid-verifier https://github.com/mozilla/browserid-verifier.git#docker-improvements
-docker build -f ./docs/self-host.docker -t fxa-profile-server https://github.com/michielbdejong/fxa-profile-server.git#docker
+docker build -f ./docs/self-host.docker -t fxa-profile-server https://github.com/mozilla/fxa-profile-server.git
 docker build -t syncserver https://github.com/mozilla-services/syncserver.git
 docker build -t syncto https://github.com/mozilla-services/syncto.git
 docker build -t fxa-self-hosting https://github.com/michielbdejong/fxa-self-hosting.git
